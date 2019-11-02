9d7c8aa5db86ace126b03b6e089acf1986fd8705
gangal.4 <gangal.4@osu.edu>
2019-11-02 18:44:19 -0400

    added file extraction from database now need transcription code

--- azureBackendwork.py
+++ azureBackendwork.py
@@ -1,31 +1,75 @@
 import mysql.connector as sql
 import azure.cognitiveservices.speech as ssdk
 #import pymongo as mongo
+import sys
+import os
 
 
-# azure configs
-speech_key = aa5972d479ec46cf8571485719c62c7d
-service_region = East US
-speech_config = speechsdk.speechConfig(subscription=speech_key, region=service_region
+def main ():
+  # azure configs
+  speech_key = aa5972d479ec46cf8571485719c62c7d
+  service_region = East US
+  speech_config = speechsdk.speechConfig(subscription=speech_key, region=service_region
 
-# DATABASE WORK :later step query database for data
+  # DATABASE WORK :later step query database for data
 
-# part 1 connect to the data base
+  # part 1 connect to the data base
+  readFile = ""
 
-#connect to database
-db = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    passwd="yourpassword"
-    database = "name of db"
-    )
+  try:
+  
+    host = ""
+    user = ""
+    passwd =""
+    database = ""
+
+    #connect to database
+    db = mysql.connector.connect(
+      host=host,
+      user=user,
+      passwd=passwd,
+      database = database)
+
+    # get the cursor 
+    cursor = db.cursor()
 
 
-cursor = db.cursor()
+    # audioFiles is a list of the audioFile location 
+    audioFiles = cursor.execute("SELECT id, audio FROM rd_tbl WHERE transcript IS NULL" )
+
+    # convert the LongBlobs ( make a func)  
+    afs = generateAudioFiles(audioFiles)
+
+    for f in afs:
+        
 
 
-# audioFiles is a list of the audioFile
-audioFiles = cursor.execute("SELECT id, audio FROM rd_tbl WHERE transcript IS NULL" )
+      
+  except:
+    print("an error occured with the audio handling")
+    
+  
+def convert_file(data,filename): 
+  with open(filename, 'wb') as file:
+    file.write(data)
+
+
+def generateAudioFiles(audioFiles):
+  genFiles = []
+  for af in audioFiles:
+    
+    num = 1
+    filename = "sample" + str(num) 
+    fullPath = os.path.dirname(os.path.realpath(".")) + filename + ".wav"
+    
+    convert_file(af,fullPath)
+    newFile = fullPath
+    
+    genFiles.append(fullPath)
+
+  return genFiles
+
+
 
 
 
