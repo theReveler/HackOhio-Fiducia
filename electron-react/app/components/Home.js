e59be4bd1513d0e1866fc59b6025d3ca80ddb10d
spewolf <spewolf@bgsu.edu>
2019-11-02 17:47:08 -0400

    Add search bar

--- electron-react/app/components/Home.js
+++ electron-react/app/components/Home.js
@@ -9,11 +9,35 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Fiducia</h2>
         <Link to={routes.RECORD}>Record a session</Link>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+        <input type="submit" value="Submit" />
+      </form>
       </div>
     );
   }
