7730e332404b7181820f2dec4ebef330b726ad8d
spewolf <spewolf@bgsu.edu>
2019-11-03 01:26:51 -0400

    Add record scaffolding

--- electron-react/app/components/Record.js
+++ electron-react/app/components/Record.js
@@ -4,35 +4,57 @@ import { Link } from 'react-router-dom';
 import styles from './Record.css';
 import routes from '../constants/routes';
 
-type Props = {
-    time: Number,
-    status: Boolean
-};
 
-export default class Record extends Component<Props> {
-    props: Props;
+export default class Record extends Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {value: '', recording: false, time: 1};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRecord = this.handleRecord.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  handleRecord(e) {
+    if(this.state.recording) {
+      this.stopRecording()
+      this.setState({ recording: false});
+      clearInterval(this.interval);
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
+    else {
+      this.startRecording()
+      this.setState({recording: true});
+      this.interval = setInterval(this.tick, 1000);
     }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
+  }
+
+  tick() {
+    console.log('tick');
+    this.setState({time: 20});
+  }
+
+  stopRecording() {
+    return undefined
+  }
+
+  startRecording() {
+    return undefined
+  }
+
+  sendData(recording) {
+    console.log(recording);
+  }
+
   render() {
-    const {
-        time = 0,
-        state = false
-    } = this.props;
     return (
         <div>
             <div className={styles.backButton} data-tid="backButton">
@@ -40,10 +62,10 @@ export default class Record extends Component<Props> {
                 <i className="fa fa-arrow-left fa-3x" />
             </Link>
             </div>
+            <h2 className={styles.title}>Record a session</h2>
             <div className={styles.recordWidget}>
-                <h2 className={styles.title}>Record a session</h2>
-                <button className={styles.recordButton}>{status ? 'Stop' : 'Start'}</button>
-                <p  className={styles.time}>{time} seconds</p>
+                <button className={styles.recordButton} onClick={this.handleRecord}>{status ? 'Stop' : 'Start'}</button>
+                <p  className={styles.time}>{this.state.time} seconds</p>
                 <p className={styles.sessionNameLabel}>Name of Session</p>
                 <form onSubmit={this.handleSubmit}>
                     <input className={styles.sessionName} type="text" value={this.state.value} onChange={this.handleChange} />
